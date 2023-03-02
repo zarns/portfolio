d8930564611678a2bcf40df071773c513bd73dd5
Mason Zarns <mason@zarns.net>
2023-03-02 16:35:25 -0600

    Updates

---
+++ precache-manifest.1bfeb3d6d22543dab24236482a5493f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e772ccc5cd6cb3a32488",
+    "url": "/portfolio/static/css/main.9b37c2c2.chunk.css"
+  },
+  {
+    "revision": "e772ccc5cd6cb3a32488",
+    "url": "/portfolio/static/js/main.f2ac331d.chunk.js"
+  },
+  {
+    "revision": "748a1fb7b6f48af76830",
+    "url": "/portfolio/static/js/runtime~main.c953b8f2.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/portfolio/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/portfolio/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "593307efd7126e6b4a4f54fce5369277",
+    "url": "/portfolio/index.html"
+  }
+];

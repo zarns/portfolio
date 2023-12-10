daee319862d44f00e349f0e742f8458697f9fc5d
Mason Zarns <mason@zarns.net>
2023-12-10 17:47:34 -0600

    Try css change

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -35,9 +35,7 @@ const Portfolio: FC = memo(() => {
             return (
               <div className="pb-6" key={`${title}-${index}`}>
                 <div
-                  className={classNames(
-                    'relative h-max w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
-                  )}>
+                  className="relative w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl">
                   <Image alt={title} layout="responsive" placeholder="blur" src={image} />
                   <ItemOverlay item={item} />
                 </div>
@@ -45,6 +43,7 @@ const Portfolio: FC = memo(() => {
             );
           })}
         </div>
+
 
         {portfolioDemosPart2.map((item, index) => {
           const {title, image} = item;
